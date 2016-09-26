ad35f9e639b817e93e163ce25e1d47a1f08f71b8
Andi <andicbuchanan@gmail.com>
2016-09-26 19:26:01 +1300

    add class and menu

--- MusicCollection/Program.cs
+++ MusicCollection/Program.cs
@@ -10,6 +10,21 @@ namespace MusicCollection
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Music Collection");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Create a record");
+            Console.WriteLine("2. View all records");
+            Console.WriteLine("3. Update a record");
+            Console.WriteLine("4. Delete a record");
+            string userChoice = Console.ReadLine();
         }
     }
+    class Albun
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+    }
 }
+
